52c56c848341702723498291de412c95be3900a6
Mayopi <cyade007@gmail.com>
2023-10-30 10:15:30 +0700

    fix: loading text center

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="w-screen h-screen flex flex-col gap-3 justify-center items-center">
-      <p className="font-semibold text-xl uppercase tracking-wider">Loading Page Content Please Wait</p>
+      <p className="font-semibold text-xl uppercase tracking-wider text-center">Loading Page Content Please Wait</p>
       <div className="loading loading-dots loading-lg text-secondary"></div>
     </div>
   );
